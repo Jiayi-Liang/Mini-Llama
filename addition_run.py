26d4e7f7fe92a63f6142b435a6279d8f19900dcb
Jiayi Liang <59698829+Jiayi-Liang@users.noreply.github.com>
2026-02-13 00:15:39 -0500

    updated function import

--- addition_run.py
+++ addition_run.py
@@ -16,7 +16,9 @@ from optimizer import AdamW
 from llama import Llama
 from config import LlamaConfig
 
+from torch.nn import functional as F
 import torch.nn.functional as F
+from torch.optim import AdamW
 from tqdm import tqdm
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
